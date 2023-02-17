992ac1a77ce14ad3a5a4afcd16213631896c7c09
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-02-17 11:50:53 +0100

    Added network code

--- My project/Assets/Scripts/Player/PlayerNetwork.cs
+++ My project/Assets/Scripts/Player/PlayerNetwork.cs
@@ -2,31 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
-
+using TMPro;
 public class PlayerNetwork : NetworkBehaviour
 {
+    [SerializeField] private TextMeshProUGUI text;
+    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private void Awake()
+    {
+        text = GameObject.Find("DebugText").GetComponent<TextMeshProUGUI>();
+    }
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (int previousValue, int newValue) => { };
+        string s = OwnerClientId + "; + randomNumber" + randomNumber.Value;
+        text.text = s;
+    }
     void Update()
     {
         if (!IsOwner) return;
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(OwnerClientId + ";" + randomNumber.Value);
+            randomNumber.Value = Random.Range(0, 100);
+        }
         Vector3 moveDir = new Vector3(0, 0, 0);
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKey(KeyCode.W))
         {
             moveDir.z += 1f;
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             moveDir.x += -1f;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             moveDir.x += 1f;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             moveDir.z += -1f;
         }
 
-        float moveSpeed = 3f;
+        float moveSpeed = 5f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 }
