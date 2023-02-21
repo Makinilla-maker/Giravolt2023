1a32a8cc59d09ff0e4665ab00157e11ff017b06c
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-02-21 12:14:25 +0100

    attempt

--- My project/Assets/Scripts/Network/PlayerCameraFix.cs
+++ My project/Assets/Scripts/Network/PlayerCameraFix.cs
@@ -5,13 +5,13 @@ using Unity.Netcode;
 using TMPro;
 public class PlayerCameraFix : NetworkBehaviour
 {
-    Camera camera1;
+    GameObject camera1;
     Camera camera2;
     Camera camera3;
     Camera camera4;
     void Awake()
     {
-        camera1 = GameObject.Find("CenterEyeAnchor").GetComponent<Camera>();
+        camera1 = GameObject.Find("CenterEyeAnchor");
         camera2 = GameObject.Find("LeftEyeAnchor").GetComponent<Camera>();
         camera3 = GameObject.Find("RightEyeAnchor").GetComponent<Camera>();
     }
@@ -24,15 +24,13 @@ public class PlayerCameraFix : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        camera1 = GameObject.Find("CenterEyeAnchor").GetComponent<Camera>();
-        camera2 = GameObject.Find("LeftEyeAnchor").GetComponent<Camera>();
-        camera3 = GameObject.Find("RightEyeAnchor").GetComponent<Camera>();
+        camera1 = GameObject.Find("CenterEyeAnchor");
+        // camera2 = GameObject.Find("LeftEyeAnchor").GetComponent<Camera>();
+        // camera3 = GameObject.Find("RightEyeAnchor").GetComponent<Camera>();
         if (!IsOwner)
         {
             Debug.Log(IsOwner);
-            camera1.enabled = false;
-            camera2.enabled = false;
-            camera3.enabled = false;
+            camera1.SetActive(false);
         }
     }
 }
