05863634702b2ce714fad9933f4529677835721b
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-02-21 14:19:28 +0100

    asd

--- My project/Assets/Scripts/Player/PlayerNetwork.cs
+++ My project/Assets/Scripts/Player/PlayerNetwork.cs
@@ -6,51 +6,12 @@ using TMPro;
 public class PlayerNetwork : NetworkBehaviour
 {
     [SerializeField] Camera camera;
-    private UiManager uiManager;
-    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-    public CharacterController controller;
-    public float speed = 12f;
-    public float gravity = -9.81f;
-    public float jump = 1f;
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-    Vector3 velocity;
-    bool isGrounded;
-    void Awake()
-    {
-        uiManager = GameObject.Find("UiManager").GetComponent<UiManager>();
-    }
-    public override void OnNetworkSpawn()
-    {
-
-    }
-    void Start()
-    {
-        uiManager.CleanAllUI();
-    }
     void Update()
     {
         if (!IsOwner)
         {
-            Debug.Log(IsOwner);
             camera.enabled = false;
         }
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if(isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
-        }
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
     }
 }
 
