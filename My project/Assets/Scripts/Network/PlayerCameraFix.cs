f0643858c761c587f22a9f3441ce360509844b67
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-02-21 12:26:02 +0100

    attempt

--- My project/Assets/Scripts/Network/PlayerCameraFix.cs
+++ My project/Assets/Scripts/Network/PlayerCameraFix.cs
@@ -5,32 +5,32 @@ using Unity.Netcode;
 using TMPro;
 public class PlayerCameraFix : NetworkBehaviour
 {
-    GameObject camera1;
+    [SerializeField] private GameObject[] camera1;
     Camera camera2;
     Camera camera3;
     Camera camera4;
-    void Awake()
-    {
-        camera1 = GameObject.Find("CenterEyeAnchor");
-        camera2 = GameObject.Find("LeftEyeAnchor").GetComponent<Camera>();
-        camera3 = GameObject.Find("RightEyeAnchor").GetComponent<Camera>();
-    }
-    // Start is called before the first frame update
     void Start()
     {
-        
+        camera1 = GameObject.FindGameObjectsWithTag("OVRCameraRig");
+        if (!IsOwner)
+        {
+            for(int i = 0; i < camera1.Length; i++)
+            {
+                camera1[i].SetActive(false);
+            }
+            
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
-        camera1 = GameObject.Find("CenterEyeAnchor");
-        // camera2 = GameObject.Find("LeftEyeAnchor").GetComponent<Camera>();
-        // camera3 = GameObject.Find("RightEyeAnchor").GetComponent<Camera>();
         if (!IsOwner)
         {
-            Debug.Log(IsOwner);
-            camera1.SetActive(false);
+            for(int i = 0; i < camera1.Length; i++)
+            {
+                camera1[i].SetActive(false);
+            }
+            
         }
     }
 }
