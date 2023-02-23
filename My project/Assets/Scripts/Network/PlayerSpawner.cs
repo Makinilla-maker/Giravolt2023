a02fbf6059bb5a2b11271addac664e7b110168f4
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-02-23 13:58:17 +0100

    ddd

---
+++ My project/Assets/Scripts/Network/PlayerSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Unity.Netcode;
+public class PlayerSpawner : NetworkBehaviour
+{
+    private TestRelay relay;
+    void Awake()
+    {
+        relay = GameObject.Find("TestRelay").GetComponent<TestRelay>();
+    }
+    public override void OnNetworkSpawn() {
+    if (IsServer)
+        relay.SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId,0);
+    else
+           relay.SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId,1);
+    }
+}
+
--- My project/Assets/Scripts/Network/TestRelay.cs
+++ My project/Assets/Scripts/Network/TestRelay.cs
@@ -12,6 +12,23 @@ using Unity.Netcode.Transports.UTP;
 public class TestRelay : MonoBehaviour
 {
     public TextMeshProUGUI joinCodeText;
+    [SerializeField] private GameObject playerPrefabA; //add prefab in inspector
+    [SerializeField] private GameObject playerPrefabB; //add prefab in inspector
+    
+    [ServerRpc(RequireOwnership=false)] //server owns this object but client can request a spawn
+    public void SpawnPlayerServerRpc(ulong clientId,int prefabId)
+    {
+        GameObject newPlayer;
+        if (prefabId==0)
+                newPlayer=(GameObject)Instantiate(playerPrefabA);
+        else
+            newPlayer=(GameObject)Instantiate(playerPrefabB);
+        NetworkObject netObj=newPlayer.GetComponent<NetworkObject>();
+        newPlayer.SetActive(true);
+        netObj.SpawnAsPlayerObject(clientId,true);
+    }
+    
+
     void Awake()
     {
         joinCodeText = GameObject.Find("JoinCode").GetComponent<TextMeshProUGUI>();
