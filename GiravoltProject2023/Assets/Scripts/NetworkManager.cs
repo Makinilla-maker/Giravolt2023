5d449ab5e6666a5f374b51dbbf9468082bd6eed7
Makinilla-maker <valverdeoriol@gmail.com>
2023-03-17 15:54:02 +0100

    online implemented

---
+++ GiravoltProject2023/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToServer();
+    }
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect To Server...");
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOption = new RoomOptions();
+        roomOption.MaxPlayers = 10;
+        roomOption.IsVisible = true;
+        roomOption.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("Room 1",roomOption, TypedLobby.Default);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("New player joined to the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
