317fe76b23ba05d72e49f0a94ed70c98c207da4f
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-05-28 17:09:37 +0200

    a

--- GiravoltProject2023/Assets/Giravolt/Scripts/Rooms/PlayerListing.cs
+++ GiravoltProject2023/Assets/Giravolt/Scripts/Rooms/PlayerListing.cs
@@ -8,19 +8,10 @@ public class PlayerListing : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
     public Player Player {get; private set;}
-<<<<<<< HEAD
-    void Awake()
-    {
-        
-    }
-    public void SetPlayerInfo(Player player)
-    {
-        Player = player;
-=======
+
     public void SetPlayerInfo(Player player)
     {
         Player = player;
         text.text = player.NickName;
->>>>>>> parent of 25d26528 (set custom name, must test online)
     }
 }
