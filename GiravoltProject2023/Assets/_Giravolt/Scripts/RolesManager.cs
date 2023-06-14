d8f22cac64036a11910682b85837e4f5a0d6da5a
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-06-14 22:05:45 +0200

    Update RolesManager.cs

--- GiravoltProject2023/Assets/_Giravolt/Scripts/RolesManager.cs
+++ GiravoltProject2023/Assets/_Giravolt/Scripts/RolesManager.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using ExitGames.Client.Photon.StructWrapping;
-
+using UnityEngine.SceneManagement;
 public class RolesManager : MonoBehaviour
 {
     [SerializeField] int id;
     [SerializeField] bool imAssassin;
     private MainConnect mC;
+    private bool doOnce = false;
     private void Start()
     {
         mC = FindObjectOfType<MainConnect>();
-        id = PhotonNetwork.LocalPlayer.ActorNumber;
+        if(SceneManager.GetActiveScene().name == "SampleScene")
+            id = PhotonNetwork.LocalPlayer.ActorNumber;
         imAssassin = false; 
         SetLocalPlayerAsAssassin();
     }
-
+    private void Update()
+    {
+        if(!doOnce)
+        {
+            id = PhotonNetwork.LocalPlayer.ActorNumber;
+            doOnce = true;
+        }
+    }
     public void SetLocalPlayerAsAssassin()
     {
         Debug.Log("FUNCTION BEING CALLED IN START");
