040408d9fc9444e77880bcbffa38c37e85ecb8c4
Makinilla-maker <60987205+Makinilla-maker@users.noreply.github.com>
2023-06-17 20:48:16 +0200

    das

--- GiravoltProject2023/Assets/_Giravolt/Scripts/LightsSwitch.cs
+++ GiravoltProject2023/Assets/_Giravolt/Scripts/LightsSwitch.cs
@@ -6,21 +6,32 @@ public class LightsSwitch : MonoBehaviour
 {
     [SerializeField] public bool areLightsOn;
     [SerializeField] private TaskManager manager;
+    bool canISwitch;
+    float time = 2;
     // Start is called before the first frame update
     void Start()
     {
         manager = GameObject.Find("TaskManager").GetComponent<TaskManager>();
         areLightsOn = true;
+        canISwitch = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!canISwitch)
+        {
+            time -= Time.deltaTime;
+            if(time <= 0)
+            {
+                canISwitch = true;
+                time = 3;
+            }
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Player_Hand"))
+        if(collision.gameObject.CompareTag("Player_Hand") && canISwitch)
         {
             switch (areLightsOn)
             {
@@ -33,6 +44,7 @@ public class LightsSwitch : MonoBehaviour
                 default:
                     break;
             }
+            canISwitch = false;
             Debug.Log("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
         }
     }
